21ac77ce1daaca2a06fbb77e2c35745dd0309c45
CheryleRg <152128183+CheryleRg@users.noreply.github.com>
2024-06-16 12:54:11 +0800

    更名

--- test/test_calcShortestPath.py
+++ test/test_calcShortestPath.py
@@ -18,10 +18,7 @@ class TestCalcShortestPath(unittest.TestCase):
         import os
         os.unlink(self.temp_file.name)
 
-    def test_1(self):
-        """
-        valid_word_in_graph
-        """
+    def test_valid_word_in_graph(self):
         # word1 和 word2 都存在于图中，且存在一条最短路径
         shortest_path, path_weight = self.graph.calcShortestPath("To", "new")
         self.assertIsNotNone(shortest_path)
@@ -35,84 +32,40 @@ class TestCalcShortestPath(unittest.TestCase):
         self.assertEqual(path_weight, 3)
         print("测试1通过！")
 
-    def test_2(self):
-        """
-        word1_not_in_graph
-        """
+    def test_word1_not_in_graph(self):
         # word1不在图中、word2存在于图中
         shortest_path, path_weight = self.graph.calcShortestPath("not_in_graph", "worlds")
         self.assertIsNone(shortest_path)
         self.assertIsNone(path_weight)
         print("测试2通过！")
 
-    def test_3(self):
-        """
-        word2_not_in_graph
-        """
-        # word1存在于图中、word2不在图中
-        shortest_path, path_weight = self.graph.calcShortestPath("To", "not_in_graph")
-        self.assertIsNone(shortest_path)
-        self.assertIsNone(path_weight)
-        print("测试3通过！")
 
-    def test_4(self):
-        """
-        words_all_not_in_graph
-        """
+    def test_words_all_not_in_graph(self):
         # word1和word2都不在图中
         shortest_path, path_weight = self.graph.calcShortestPath("not_in_graph", "not_in_graph_")
         self.assertIsNone(shortest_path)
         self.assertIsNone(path_weight)
         print("测试4通过！")
 
-    def test_5(self):
-        """
-        empty_word1
-        """
+    def test_empty_word1(self):
         # word1为空字符串
         shortest_path, path_weight = self.graph.calcShortestPath("", "worlds")
         self.assertIsNone(shortest_path)
         self.assertIsNone(path_weight)
         print("测试5通过！")
 
-    def test_6(self):
-        """
-        empty_word2
-        """
-        # word2为空字符串
-        shortest_path, path_weight = self.graph.calcShortestPath("To", "")
-        self.assertIsNone(shortest_path)
-        self.assertIsNone(path_weight)
-        print("测试6通过！")
-
-    def test_7(self):
-        """
-        empty_both_words
-        """
+    def test_empty_both_words(self):
         # word1和word2都为空字符串 ""
         shortest_path, path_weight = self.graph.calcShortestPath("", "")
         self.assertIsNone(shortest_path)
         self.assertIsNone(path_weight)
-        print("测试7通过！")
+        print("测试6通过！")
 
-    def test_8(self):
-        """
-        invalid_word
-        """
-        # 单词包含非字母字符的情况
-        shortest_path, path_weight = self.graph.calcShortestPath("To@", "worlds")
-        self.assertIsNone(shortest_path)
-        self.assertIsNone(path_weight)
-        print("测试8通过！")
-
-    def test_9(self):
-        """
-        no_path_exists
-        """
+    def test_no_path_exists(self):
         shortest_path, path_weight = self.graph.calcShortestPath("civilizations", "strange")
         self.assertIsNone(shortest_path)
         self.assertIsNone(path_weight)
-        print("测试9通过！")
+        print("测试7通过！")
 
 
 if __name__ == '__main__':
